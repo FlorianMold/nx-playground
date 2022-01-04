c183c1faf3a0da408412b9ed42afb3e420a0d44b
Florian Mold <florian.mold@agnet.at>
2022-01-04 14:59:07 +0100

    feat(apps/todos/app): display todo items

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  title: string;
+}
+
 @Component({
   selector: 'myorg-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todos';
+
+  static uniqueId = 3;
+
+  todos: Todo[] = [
+    { title: 'Todo 1' },
+    { title: 'Todo 2' }
+  ];
+
+  addTodo() {
+    this.todos.push({
+      title: `Todo ${AppComponent.uniqueId++}`
+    })
+  }
 }
